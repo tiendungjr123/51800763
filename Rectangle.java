7f208742c977a376795f353506c3b256ce8fdcc9
51800023 <ngotandat93thcslt99@gmail.com>
2020-10-30 14:38:24 +0700

    File nay cua Dat

---
+++ Rectangle.java
@@ -0,0 +1,59 @@
+public class Rectangle
+{
+	private float length = 1.0f;
+	private float width = 1.0f;
+	
+	public Rectangle()
+	{
+		length=1.0f;
+		width=1.0f;
+	}
+	
+	public Rectangle(float length, float width)
+	{
+		this.length = length;
+		this.width=width;
+	}
+	
+	public float getLength()
+	{
+		return length;
+	}
+	
+	public void setLength(float length)
+	{
+		this.length=length;
+	}
+	
+	public float getWidth()
+	{
+		return width;
+	}
+	
+	public void setWidth(float width)
+	{
+		this.width=width;
+	}
+	
+	public double getArea()
+	{
+		return length*width;
+	}
+	
+	public double getPerimeter()
+	{
+		return (length+width)*2;
+	}
+	
+	public String toString()
+	{
+		return "Rectangle[length= "+length+ " , width= "+width+" ]";
+		
+	}
+	
+	public void print()
+	{
+		System.out.format("Length= %f and Width= %f \n",length,width);
+	}
+	
+}
